f419a4b281c40c84eb89e561a7eaca65ff10c48b
Pasha Sakhanda <pasha.sakhanda@gmail.com>
2018-02-20 10:53:05 +0200

    first commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+	<footer class="footer" style="opacity:0.9" >
+		<div class="container">
+			<div class="content has-text-centered">
+				<span>
+					<a href="https://www.facebook.com/groups/245221019171012/"><img src="/img/fb-icon.png" width="35px" style="margin-right: 20px"></a>
+					<a href="mailto:tcceramic2@gmail.com"><img src="/img/gmail-icon.png" width="35px" style="margin-left: 20px"></a>
+				</span><br/>
+				<span>
+					<a href="/index.php" style="margin-right: 15px; font-size: 10px; color:#FFB800">ГОЛОВНА</a>
+					<a href="/about.php" style="margin-right: 40px; margin-left: 40px; font-size: 10px; color:#FFB800">ПРО НАС</a>
+					<a href="/contacts.php" style="margin-left: 15px; font-size: 10px; color:#FFB800">КОНТАКТИ</a>
+				</span><br/>
+				<span style="margin: 0; font-size:10px; margin-right: 10px">&#xA9; 2018 
+					<a href="/index.php" style="color: #4a4a4a; margin-left: 5px">TC CERAMIC</a>
+				</span>
+			</div>
+		</div>
+	</footer>
