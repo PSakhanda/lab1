f32cd59c8929c49dee146eb50ffa71d8bc92a992
Pasha Sakhanda <pasha.sakhanda@gmail.com>
2018-02-20 10:55:31 +0200

    third commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+	<title>TC Ceramic - Надсучасні енергозберігаючі технології</title>
+</head>
+<body>
+	<?php include "header.php"; ?>
+
+	<?php include "footer.php"; ?>
+</body>
+</html>
