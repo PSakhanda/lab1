3db3b5528cd6830133cc790a7f04259b2eec806a
Pasha Sakhanda <pasha.sakhanda@gmail.com>
2018-02-20 10:53:43 +0200

    second commit

---
+++ header.php
@@ -0,0 +1,23 @@
+<nav class="navbar is-black" role="navigation" aria-label="main navigation" style="opacity: 0.9">
+		<div class="navbar-brand">
+			<a class="navbar-item" href="/index.php" >
+				<img src="/img/logo-navbar.png" alt="TC Ceramic - Надсучасні енергозберігаючі технології">
+			</a>
+		</div>
+		<div class="navbar-menu">
+			<div class="navbar-start">
+				<a class="navbar-item" href="/index.php">ГОЛОВНА</a>
+				<div class="navbar-item has-dropdown is-hoverable">
+					<a class="navbar-link">ХАРАКТЕРИСТИКИ МАТЕРІАЛУ</a>
+					<div class="navbar-dropdown">
+						<a class="navbar-item" href="/main_characteristics.php"><strong>ОСНОВНІ ХАРАКТЕРИСТИКИ</strong></a>
+						<a class="navbar-item" href="/action.php"><strong>ПРИНЦИП ДІЇ</strong></a>
+						<a class="navbar-item" href="/tech_charasteristics.php"><strong>ТЕХНІЧНІ ХАРАКТЕРИСТИКИ</strong></a>
+					</div>
+				</div>
+				<a class="navbar-item" href="/advantages.php">ПЕРЕВАГИ МАТЕРІАЛУ</a>
+				<a class="navbar-item" href="/about.php">ПРО НАС</a>
+				<a class="navbar-item" href="/contacts.php">КОНТАКТИ</a>
+			</div>
+		</div>
+	</nav>
